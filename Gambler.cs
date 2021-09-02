379e1fd4392d42eb6c9dbe65ff12ccb35d5fb672
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-02 10:46:48 +0530

    [Rupali]Feat:uploaded first UC

---
+++ Gambler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Gambler
+    {
+        public void gambling()
+        {
+            const int gamestake = 100;
+            const int gamebet = 1;
+            Console.WriteLine("Initial value:\t Stake=${0}\t Bet per game=${1}",gamestake,gamebet);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Gambling Program!");
+            Gambler gb = new Gambler();
+            gb.gambling();
+        }
+    }
+}
